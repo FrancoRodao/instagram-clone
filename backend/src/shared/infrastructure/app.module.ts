618f508233f67b694c8ff06715e9d5cd17226241
FrancoRodao <francorodao802@gmail.com>
2024-11-22 23:44:47 -0300

    feat(backend/shared): add posts module to app module

--- backend/src/shared/infrastructure/app.module.ts
+++ backend/src/shared/infrastructure/app.module.ts
@@ -1,11 +1,11 @@
 import { type MiddlewareConsumer, Module, type NestModule } from '@nestjs/common'
-import { UsersModule } from '../../users/infrastructure'
-import { AuthModule } from '../../auth/infrastructure'
-import { I18NMiddleware } from '../../i18n/infrastructure/i18n.middleware'
-import { I18NModule } from '../../i18n/infrastructure/i18n.module'
-
+import { AuthModule } from '../../auth/infrastructure/auth.module'
+import { UsersModule } from '../../users/infrastructure/users.module'
+import { I18NModule } from '../../shared/services/i18n/infrastructure/i18n.module'
+import { I18NMiddleware } from '../services/i18n/infrastructure/i18n.middleware'
+import { PostsModule } from '../../posts/infrastructure/posts.module'
 @Module({
-  imports: [I18NModule, UsersModule, AuthModule]
+  imports: [AuthModule, I18NModule, UsersModule, PostsModule]
 })
 export class AppModule implements NestModule {
   configure (consumer: MiddlewareConsumer): void {
